22ce2db31dcf22e56f13526f57a8a563c411447a
knoddin <knoddin@gmail.com>
2016-10-01 19:30:56 -0400

    corrected misspelling of inverse.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,4 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user, inverse_of: :reservations
-  belongs_to :week, inversse_of: :reservations
+  belongs_to :week, inverse_of: :reservations
 end
