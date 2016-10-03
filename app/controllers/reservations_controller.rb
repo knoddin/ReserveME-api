1ff63ba5c09412ef7fe60a6e7e299de8f0e0b6f9
knoddin <knoddin@gmail.com>
2016-10-02 22:42:49 -0400

    tried having reservations controller inherit from protected controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,4 @@
-class ReservationsController < ApplicationController
+class ReservationsController < ProtectedController
   before_action :set_reservation, only: [:show, :update, :destroy]
 
   # GET /reservations
