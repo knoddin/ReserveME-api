e094f761ef5ba7b238e455b4800ebb071e174e55
knoddin <knoddin@gmail.com>
2016-10-11 19:54:48 -0400

    changed validations
    changed validations in reservation model to include user id.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ActiveRecord::Base
   belongs_to :user
+  validates :user_id, presence: true
   validates :end_date, date: { after: :start_date}
   validates :start_date, date: { after: Proc.new { Time.now } }
 end
